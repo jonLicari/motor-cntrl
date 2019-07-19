3f5afe7e0dec9058e3e4c5971a09e087c4c4c889
Jonathan Licari <jlicari@ryerson.ca>
2019-07-19 01:53:36 -0400

    Update demo_v3.h

--- demo_v3.h
+++ demo_v3.h
@@ -6,22 +6,30 @@
 #ifndef demo_v3_h
 #define demo_v3_h
 
- 
-// Define the expected analog values for the Flight Mode Select button
-#define exp0 1014
-#define exp1 974
-#define exp2 952
-#define exp3 931
+  // Define the expected analog values for the Flight Mode Select button
+  #define exp0 1014
+  #define exp1 974
+  #define exp2 952
+  #define exp3 931
 
- // Pin Assignments
-const int joy = A0; 
-const int throttle = A1;
-const int pedal = A2;
-const int mode = A3;
+  // Pin Assignments
+  const int joy = A0; 
+  const int throttle = A1;
+  const int pedal = A2;
+  const int mode = A3;
 
-// Motor PWM pin declaration
-const int m1 = 9; // L motor
-const int m2 = 10; // T motor
-const int m3 = 11; // R motor
+  // Motor PWM pin declaration
+  const int m1 = 9; // L motor
+  const int m2 = 10; // T motor
+  const int m3 = 11; // R motor
+
+  // Analog variables
+  int throttle_out = 0;
+  int pedal_out = 0;
+  int joy_out = 0;
+
+  // Miscellaneous 
+  float op1, op2, op3, ftr;
+  int old, sel, select; // Flight mode select variable
 
 #endif
